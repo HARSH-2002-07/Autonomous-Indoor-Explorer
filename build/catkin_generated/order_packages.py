aa94601bfa11b497e989323817e37e1fda251f0e
Harsh <hj075028@gmail.com>
2025-08-18 12:49:59 +0530

    Stage 1: Setup workspace and base package for Autonomous Explorer (ROS1)

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/foxy/major_project_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/foxy/ros_projects_ws/devel;/home/foxy/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/foxy/ros_projects_ws/devel;/home/foxy/catkin_ws/devel;/opt/ros/noetic' != '' else []
