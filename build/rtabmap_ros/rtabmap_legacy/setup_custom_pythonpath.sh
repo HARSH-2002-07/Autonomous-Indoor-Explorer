a891b7c513f2f5ee2eeb25e6ee9504a922d8d827
Harsh <hj075028@gmail.com>
2025-08-20 17:07:18 +0530

    "Rtab map removed it is throwing errors"

--- build/rtabmap_ros/rtabmap_legacy/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/foxy/major_project_ws/devel/lib/python3/dist-packages:$PYTHONPATH
-exec /usr/bin/python3 "$@"
